7c87919d6e4a9ba963c0aaebb2926d9bfb4f027e
Xinyu Chen <1546683151cxy@gmail.com>
2023-02-28 02:18:10 -0500

    First commit

---
+++ schema.sql
@@ -0,0 +1,127 @@
+DROP DATABASE IF EXISTS photoshare;
+CREATE DATABASE IF NOT EXISTS photoshare;
+USE photoshare;
+#DROP TABLE IF EXISTS Pictures CASCADE;
+#DROP TABLE IF EXISTS Users CASCADE;
+
+CREATE TABLE Users (
+    uid int4  NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(10) NOT NULL,
+    last_name VARCHAR(10) NOT NULL,
+    birth DATE NOT NULL,
+    hometown VARCHAR(20),
+    gender VARCHAR(5),
+  CONSTRAINT users_pk PRIMARY KEY (uid)
+);
+
+CREATE TABLE Albums
+(
+  aid INTEGER AUTO_INCREMENT,
+  aname VARCHAR(20) NOT NULL,
+  adate DATETIME DEFAULT CURRENT_TIMESTAMP,
+  uid INTEGER NOT NULL,
+  PRIMARY KEY (aid),
+  FOREIGN KEY (uid) REFERENCES Users (uid)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE Pictures
+(
+  pid int4  AUTO_INCREMENT,
+  uid int4,
+  imgdata longblob,
+  caption VARCHAR(255),
+  aid int4 NOT NULL,
+  INDEX upid_idx (uid),
+  CONSTRAINT pictures_pk PRIMARY KEY (pid),
+  FOREIGN KEY (aid) REFERENCES Albums (aid)
+  ON DELETE CASCADE,
+  FOREIGN KEY (uid) REFERENCES Users (uid)
+);
+
+CREATE TABLE Comments
+(
+  cid INTEGER AUTO_INCREMENT,
+  text TEXT NOT NULL,
+  cdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+  uid INTEGER NOT NULL,
+  pid INTEGER NOT NULL,
+  PRIMARY KEY (cid),
+  FOREIGN KEY (pid) REFERENCES Pictures (pid)
+  ON DELETE CASCADE,
+  FOREIGN KEY (uid) REFERENCES Users (uid)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE has
+(
+  uid INTEGER NOT NULL,
+  aid INTEGER,
+  PRIMARY KEY (uid, aid),
+  FOREIGN KEY (uid) REFERENCES Users (uid)
+  ON DELETE CASCADE,
+  FOREIGN KEY (aid) REFERENCES Albums (aid)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE contains
+(
+  aid INTEGER NOT NULL,
+  pid INTEGER,
+  FOREIGN KEY (aid) REFERENCES Albums (aid)
+  ON DELETE CASCADE,
+  FOREIGN KEY (pid) REFERENCES Pictures (pid)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE Tags
+(
+  name VARCHAR(20),
+  tid INTEGER,
+  PRIMARY KEY (tid)
+);
+
+CREATE TABLE relate_to
+(
+  pid INTEGER,
+  tid INTEGER,
+  PRIMARY KEY (pid, tid),
+  FOREIGN KEY (pid) REFERENCES Pictures (pid),
+  FOREIGN KEY (tid) REFERENCES Tags (tid)
+);
+
+CREATE TABLE makes
+(
+  uid INTEGER NOT NULL,
+  cid INTEGER,
+  PRIMARY KEY (uid, cid),
+  FOREIGN KEY (uid) REFERENCES Users (uid)
+  ON DELETE CASCADE,
+  FOREIGN KEY (cid) REFERENCES Comments (cid)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE is_friend
+(
+  uid1 INTEGER NOT NULL,
+  uid2 INTEGER NOT NULL,
+  CHECK (uid1 <> uid2),
+  PRIMARY KEY (uid1, uid2),
+  FOREIGN KEY (uid1) REFERENCES Users (uid) 
+  ON DELETE CASCADE,
+  FOREIGN KEY (uid2) REFERENCES Users (uid) 
+  ON DELETE CASCADE
+);
+
+CREATE TABLE likes
+(
+  pid INTEGER NOT NULL,
+  uid INTEGER NOT NULL,
+  PRIMARY KEY (pid, uid),
+  FOREIGN KEY (pid) REFERENCES Pictures (pid) 
+  ON DELETE CASCADE,
+  FOREIGN KEY (uid) REFERENCES Users (uid) 
+  ON DELETE CASCADE
+);
